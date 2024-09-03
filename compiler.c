78f3d821a2aad48beb8559c5bbc86737cc739462
mrtea9 <ionmeriacri111@gmail.com>
2024-09-03 10:50:25 +0300

    Added var declarations

--- compiler.c
+++ compiler.c
@@ -142,6 +142,19 @@ static void declaration();
 static ParseRule* getRule(TokenType type);
 static void parsePrecedence(Precedence precedence);
 
+static uint8_t identifierConstant(Token* name){
+	return makeConstant(OBJ_VAL(copyString(name->start, name->length)));
+}
+
+static uint8_t parseVariable(const char* errorMessage){
+	consume(TOKEN_IDENTIFIER, errorMessage);
+	return identifierConstant(&parser.previous);
+}
+
+static void defineVariable(uint8_t global){
+	emitBytes(OP_DEFINE_GLOBAL, global);
+}
+
 static void binary(){
 	TokenType operatorType = parser.previous.type;
 	ParseRule* rule = getRule(operatorType);
@@ -185,6 +198,15 @@ static void string(){
 	emitConstant(OBJ_VAL(copyString(parser.previous.start + 1, parser.previous.length - 2)));
 }
 
+static void namedVariable(Token name){
+	uint8_t arg = identifierConstant(&name);
+	emitBytes(OP_GET_GLOBAL, arg);
+}
+
+static void variable(){
+	namedVariable(parser.previous);
+}
+
 static void unary(){
 	TokenType operatorType = parser.previous.type;
 	
@@ -212,14 +234,14 @@ ParseRule rules[] = {
     [TOKEN_SLASH]         = {NULL,     binary, PREC_FACTOR},
     [TOKEN_STAR]          = {NULL,     binary, PREC_FACTOR},
     [TOKEN_BANG]          = {unary,    NULL,   PREC_NONE},
-    [TOKEN_BANG_EQUAL]    = {NULL,     NULL,   PREC_NONE},
+    [TOKEN_BANG_EQUAL]    = {NULL,     binary, PREC_EQUALITY},
     [TOKEN_EQUAL]         = {NULL,     NULL,   PREC_NONE},
-    [TOKEN_EQUAL_EQUAL]   = {NULL,     binary, PREC_NONE},
-    [TOKEN_GREATER]       = {NULL,     binary, PREC_NONE},
-    [TOKEN_GREATER_EQUAL] = {NULL,     binary, PREC_NONE},
-    [TOKEN_LESS]          = {NULL,     binary, PREC_NONE},
-    [TOKEN_LESS_EQUAL]    = {NULL,     binary, PREC_NONE},
-    [TOKEN_IDENTIFIER]    = {NULL,     NULL,   PREC_NONE},
+    [TOKEN_EQUAL_EQUAL]   = {NULL,     binary, PREC_EQUALITY},
+    [TOKEN_GREATER]       = {NULL,     binary, PREC_COMPARISON},
+    [TOKEN_GREATER_EQUAL] = {NULL,     binary, PREC_COMPARISON},
+    [TOKEN_LESS]          = {NULL,     binary, PREC_COMPARISON},
+    [TOKEN_LESS_EQUAL]    = {NULL,     binary, PREC_COMPARISON},
+    [TOKEN_IDENTIFIER]    = {variable, NULL,   PREC_NONE},
     [TOKEN_STRING]        = {string,   NULL,   PREC_NONE},
     [TOKEN_NUMBER]        = {number,   NULL,   PREC_NONE},
     [TOKEN_AND]           = {NULL,     NULL,   PREC_NONE},
@@ -267,19 +289,70 @@ static void expression(){
 	parsePrecedence(PREC_ASSIGNMENT);
 }
 
+static void varDeclaration(){
+	uint8_t global = parseVariable("Expect variable name.");
+	
+	if(match(TOKEN_EQUAL)){
+		expression();
+	} else {
+		emitByte(OP_NIL);
+	}
+	consume(TOKEN_SEMICOLON, "Expect ';' after variable declaration.");
+	
+	defineVariable(global);
+}
+
+static void expressionStatement(){
+	expression();
+	consume(TOKEN_SEMICOLON, "Expect ';' after expression.");
+	emitByte(OP_POP);
+}
+
 static void printStatement(){
 	expression();
 	consume(TOKEN_SEMICOLON, "Expect ';' after value.");
 	emitByte(OP_PRINT);
 }
 
+static void synchronize(){
+	parser.panicMode = false;
+	
+	while(parser.current.type != TOKEN_EOF) {
+		if(parser.previous.type == TOKEN_SEMICOLON) return;
+		switch(parser.current.type){
+			case TOKEN_CLASS:
+			case TOKEN_FUN:
+			case TOKEN_VAR:
+			case TOKEN_FOR:
+			case TOKEN_IF:
+			case TOKEN_WHILE:
+			case TOKEN_PRINT:
+			case TOKEN_RETURN:
+				return;
+				
+			default:
+				; // DO nothing
+		}
+		
+		advance();
+	}
+}
+
 static void declaration(){
-	statement();
+	if(match(TOKEN_VAR)){
+		varDeclaration();
+	} else {
+		statement();
+	}
+	
+	if(parser.panicMode) synchronize();
 }
 
 static void statement(){
 	if(match(TOKEN_PRINT)){
 		printStatement();
+	} else {
+		expressionStatement();
 	}
 }
 
--- vm.c
+++ vm.c
@@ -31,10 +31,12 @@ static void runtimeError(const char* format, ...){
 void initVM(){
 	resetStack();
 	vm.objects = NULL;
+	initTable(&vm.globals);
 	initTable(&vm.strings);
 }
 
 void freeVM(){
+	freeTable(&vm.globals);
 	freeTable(&vm.strings);
 	freeObjects();
 }
@@ -74,7 +76,17 @@ static void concatenate(){
 static InterpretResult run(){
 	#define READ_BYTE() (*vm.ip++)
 	#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
-	#define BINARY_OP(valueType, op) do { if(!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) { runtimeError("Operands must be numbers."); return INTERPRET_RUNTIME_ERROR; } double b = AS_NUMBER(pop()); double a = AS_NUMBER(pop()); push(valueType(a op b)); } while(false)
+	#define READ_STRING() AS_STRING(READ_CONSTANT())
+	#define BINARY_OP(valueType, op) \
+	do { \
+		if(!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) { \
+		runtimeError("Operands must be numbers."); \
+		return INTERPRET_RUNTIME_ERROR; \
+		} \
+	double b = AS_NUMBER(pop()); \
+	double a = AS_NUMBER(pop()); \
+	push(valueType(a op b)); \
+	} while(false)
 	
 	for(;;){
 		#ifdef DEBUG_TRACE_EXECUTION
@@ -98,6 +110,23 @@ static InterpretResult run(){
 			case OP_NIL: push(NIL_VAL); break;
 			case OP_TRUE: push(BOOL_VAL(true)); break;
 			case OP_FALSE: push(BOOL_VAL(false)); break;
+			case OP_POP: pop(); break;
+			case OP_GET_GLOBAL: {
+				ObjString* name = READ_STRING();
+				Value value;
+				if(!tableGet(&vm.globals, name, &value)){
+					runtimeError("Undefined variable '%s'.", name->chars);
+					return INTERPRET_RUNTIME_ERROR;
+				}
+				push(value);
+				break;
+			}
+			case OP_DEFINE_GLOBAL: {
+				ObjString* name = READ_STRING();
+				tableSet(&vm.globals, name, peek(0));
+				pop();
+				break;
+			}
 			case OP_EQUAL: {
 				Value b = pop();
 				Value a = pop();
@@ -146,6 +175,7 @@ static InterpretResult run(){
 	
 	#undef READ_BYTE
 	#undef READ_CONSTANT
+	#undef READ_STRING
 	#undef BINARY_OP
 }
 
